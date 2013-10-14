83d3d4c51c924958c9786fdc38dc222b4a795385
etenesaca <edgartenesaca@gmail.com>
2013-10-14 05:11:43 -0500

    Implementada una funcion para completar cadenas

--- hupernikao.js
+++ hupernikao.js
@@ -1,27 +1,41 @@
-complete_string = function(string_base){
-    
-}
+/*
+Esta funcion sirver para completar una cadena con un numero de ceros hasta que tenga un tamanio
 
+@str = Cadena que se va completar
+@num (Opcional) = Numero de caracteres que tiene que tener la cadena final, valor por defecto es 2
+@right (opcional) = Indica si se debe llenar a lado Derecho o No, valor por defecto es Falso
+@character (opcional) = Es el caracter de relleno, valor por defecto es '0'
+*/
+complete_string = function(str,num,right,character){
+    str = String(str);
+    num = num || 2;
+    num = parseInt(num);
+    character = character || '0';
+    character = String(character);
+    while(str.length < num){
+        if (right==true)
+            str = str + character
+        else
+            str = character + str
+    }
+    return str;
+}
+/*
+Devuelve la fecha y hora actual en un formato String
+*/
 getDateTime = function() {
     var date = new Date();
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
+    var hour = complete_string(date.getHours())
+    var min = complete_string(date.getMinutes())
+    var sec = complete_string(date.getSeconds())
+    var milisec = complete_string(date.getMilliseconds(),3)
 
     var year = date.getFullYear();
+    var month = complete_string(date.getMonth())
+    var day = complete_string(date.getDate())
 
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
+    var timezone = date.toString().split(" ")[5];
 
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year+"-"+month+"-"+day+" "+hour+":"+min+":"+sec+':'+date.getMilliseconds();
-
+    return year+"-"+month+"-"+day+" "+hour+":"+min+":"+sec+':'+milisec+" "+timezone;
 }
